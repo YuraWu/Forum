27022431d02af39b28770e6eda7f08ae83b704fe
SilentMajority <silent_majority@163.com>
2014-06-23 13:56:31 +0800

    冲突提交
    数据库改过，我的密码

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
-	$con = mysql_connect("localhost","root","15651803502");
+	$con = mysql_connect("localhost","root","255930");
 	mysql_select_db("forum", $con); 
 
 	function user_register($username,$password,$registerDate){
